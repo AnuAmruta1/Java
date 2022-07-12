43365b39ed377c2df951117bee409062a565a668
AnuAmruta1 <108450343+AnuAmruta1@users.noreply.github.com>
2022-07-12 12:29:47 +0530

    Add files via upload

---
+++ ArithmaticOperator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class ArithmaticOperator
+	{
+		public static void main(String[] args)
+		{
+			System.out.println("ArithmaticOperator:");
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter the Int a:");
+			int a=s.nextInt();
+			System.out.println("Int a:"+a);
+			System.out.println("Enter the Int b:");
+			int b=s.nextInt();
+			System.out.println("Int b:"+b);
+			System.out.cd cdprintln("a+b:"+(a+b));
+			System.out.println("a-b:"+(a-b));	
+			System.out.println("a*b:"+(a*b));
+			System.out.println("a/b:"+(a/b));
+		}
+	}
