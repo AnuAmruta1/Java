a1cb8c3c2035deb8bd9b0186a70e03adce8bd4d2
AnuAmruta1 <108450343+AnuAmruta1@users.noreply.github.com>
2022-07-13 19:38:34 +0530

    Add files via upload

---
+++ AscendingDesendingOrder.java
@@ -0,0 +1,20 @@
+class AscendingDesendingOrder
+ {
+	public static void main(String[] args)
+	{
+		System.out.println("Ascending order");
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+		
+		System.out.println("Decending order");
+		for(int j=10; j>=1; j--)
+		{
+			System.out.println(j);
+		}
+	}
+}
+ 
+
+	
